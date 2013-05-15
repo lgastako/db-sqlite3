26684c4d2bd93689a9d8f35916d3efb9f10e88be
John Evans <lgastako@gmail.com>
2013-05-14 21:56:21 -0700

    Reset version number.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import find_packages
 
 if __name__ == "__main__":
     setup(name="db-sqlite3",
-          version="0.0.8",
+          version="0.0.1",
           description="sqlite3 driver for db",
           author="John Evans",
           author_email="lgastako@gmail.com",
